f3eab442caf5dd39de9591889063e3defd3cb5ac
Francis Siefken <fsiefken@gmail.com>
2011-02-20 17:43:56 +0100

    Initial commit Stories project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stories::Application.config.secret_token = '19a7511210a8490ddf81d5b0b2583973c5fbdda3c6855811bfb26b4bad368e633d3d1f858b176e9637ab183a31dedd2bcbbd778ebc47b0935cdaae1f740e596a'
